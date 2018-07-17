dc09b4da213026953f2a648aded3af09b37bcf89
Frank <afrank@atcc.org>
2018-07-17 12:28:31 -0400

    moved ggdc-submit contents into ggdc-robot

--- src/ggdc-robot.py
+++ src/ggdc-robot.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 # Controller for automatically submitting jobs to the Genome-to-Genome Distance
 # Calculator (GGDC) website: https://ggdc.dsmz.de/ggdc.php, using GGDC v2.1.
 # Updated and generalized version of scripts originally developed by Katya
 # McGough at American Type Culture Collection.
-
-# todo
-# - implement "holding pattern" mode (waiting until ggdc server load dips below 100% before submitting next job)
-# - replace subprocess.call shell = true with safer method
-# - create new main script called ggdc-robot to act as executive script
 
 # DEPENDENCIES
 
@@ -20,7 +15,8 @@ import sys
 import itertools
 import numpy
 import time
-import subprocess
+import mechanize
+from bs4 import BeautifulSoup
 
 # COMMAND LINE ARGUMENTS
 
@@ -65,7 +61,7 @@ parser.add_argument('--reffile','-r',
                     required = '--queryfile' in sys.argv)
 parser.add_argument('--bruteforce', '-f',
                     help = ('enable brute force mode; this mode forces '
-                    'ggdc-robot to submit jobs to the GGDC server when the '
+                    'ggdc-robot to submit jobs to the GGDC server even when the'
                     'server load is at 100%; ATTENTION: jobs may fail due to '
                     'GGDC job queue limits'),
                     action = 'store_true')
@@ -160,6 +156,17 @@ def write_submission_files(pairs_dict, submissions_dir, maxrefs):
             files_dict[qfile_name] = rfile_name
     return(files_dict)
 
+def check_submission_format(file):
+    if os.path.isfile(file):
+        with open(file) as infile:
+            first_line = infile.readline().rstrip()
+        if os.path.isfile(first_line):
+            return('filepath')
+        else:
+            return('accession')
+    else:
+        sys.exit(file + ' not found.')
+
 def get_ggdc_status(url):
     # browse to GGDC website
     br = mechanize.Browser()
@@ -171,6 +178,76 @@ def get_ggdc_status(url):
     status_message = 'Current GGDC server load:' + status
     return(status_message)
 
+def submit_file_values(file, type, format):
+    with open(file) as infile:
+        lines = infile.read().splitlines()
+    if format == 'accession':
+        if type == 'query':
+            control = 'targetName'
+            sep = ' '
+        elif type == 'ref':
+            control = 'refGenbank'
+            sep = '\r\n'
+        submission = sep.join(lines)
+        try:
+            br.form.set_value(submission, control)
+            #print(submission)
+        except:
+            print('Error submitting ' + submission + ' ' + type + '.')
+    elif format == 'filepath':
+        if type == 'query': name = 'targetGenome'
+        elif type == 'ref': name = 'multipleRefGenomes[]'
+        for submission in lines:
+            # ----- START ERROR FOR MULTIPLE REF FASTAS ----- #
+            if len(lines) > 1:
+                sys.exit('Sorry, submission of multiple reference fasta files '
+                         'is not yet supported. Exiting.')
+            else:
+            # ----- END ERROR FOR MULTIPLE REF FASTAS ----- #
+                try:
+                    br.form.add_file(open(submission),
+                                     'text/plain',
+                                     submission,
+                                     name = name)
+                except:
+                    print('Error submitting ' + submission + ' ' + type + '.')
+    else: sys.exit('Unable to submit ' + type + ' ' + format + '. Exiting.')
+
+def ggdc_submit(url, email, blastVariant, queryfile, reffile):
+    # browse to GGDC website
+    br = mechanize.Browser()
+    br.open(url)
+
+    # begin filling out GGDC form
+    br.select_form('Form')                       # GGDC form name is just "Form"
+    br.form.set_value(email, 'email')            # fill in email accession form
+    br.form.set_value(blastVariant, 'blastVariant') # fill in BLAST method form
+
+    # fill in query form per format submitted
+    queryformat = check_submission_format(queryfile)
+    submit_file_values(file = queryfile,
+                       type = 'query',
+                       format = queryformat)
+
+    # fill in ref form per format submitted
+    refformat = check_submission_format(reffile)
+    submit_file_values(file = reffile,
+                       type = 'ref',
+                       format = refformat)
+
+    # submit GGDC job
+    submission = br.submit()
+
+    # get GGDC job response
+    submission_html = BeautifulSoup(submission, 'html.parser')
+    try:
+        response_html = submission_html.find('div', {'class': 'alert alert-success'})
+        response = response_html.get_text()
+    except:
+        response_html = submission_html.find('div', {'class': 'panel-body alert-danger'})
+        response = response_html.get_text()
+    print(response)
+
 # iteratively submits each qfile-rfile pair to GGDC using ggdc-crawler.py;
 # currently pauses for 25 minutes every 6th submission
 def submit_ggdc_jobs(bruteforce, wait, status_path, submit_path, files_dict, email, blastVariant):
@@ -178,7 +255,6 @@ def submit_ggdc_jobs(bruteforce, wait, status_path, submit_path, files_dict, ema
     jobs_requested = len(files_dict)
     print('Jobs requested = ' + str(jobs_requested))
     for job_count, (qfile, rfile) in enumerate(files_dict.items()):
-#       status = subprocess.check_output(['python', status_path], shell = True)
         status = get_ggdc_status(url)
         print(status)
         if bruteforce is not None:
@@ -198,9 +274,11 @@ def submit_ggdc_jobs(bruteforce, wait, status_path, submit_path, files_dict, ema
                submission_count = 0
                time.sleep(wait[0] * 60)
         if job_count <= jobs_requested:
-            submission = subprocess.check_output(['python', submit_path,
-                                                 email, blastVariant, qfile, rfile],
-                                                 shell = True)
+            submission = ggdc_submit(url,
+                                     email,
+                                     blastVariant,
+                                     queryfile,
+                                     reffile)
             job_count += 1
             submission_count += 1
             if 'Dear User,' in submission:
@@ -218,35 +296,38 @@ def submit_ggdc_jobs(bruteforce, wait, status_path, submit_path, files_dict, ema
             print(('Error with GGDC job ' + job_count +
                    '. Skipping to the next job.'))
 
-# SCRIPT
+def main(args):
 
-# status_path = os.path.join(get_script_path(), 'ggdc-status.py')
-submit_path = os.path.join(get_script_path(), 'ggdc-submit.py')
-submissions_dir = os.path.join(get_script_path(), 'submissions')
-if not os.path.exists(submissions_dir): os.makedirs(submissions_dir)
+    submissions_dir = os.path.join(get_script_path(), 'submissions')
+    if not os.path.exists(submissions_dir): os.makedirs(submissions_dir)
 
-email = args.email
-blastVariant = args.blastVariant
-queryfile = args.queryfile
-reffile = args.reffile
-samplefile = args.samplefile
+    email = args.email
+    blastVariant = args.blastVariant
+    queryfile = args.queryfile
+    reffile = args.reffile
+    samplefile = args.samplefile
 
-bruteforce = args.bruteforce
-wait = args.wait
+    bruteforce = args.bruteforce
+    wait = args.wait
 
-maxrefs = 75
+    maxrefs = 75
 
-if args.queryfile and args.reffile is not None:
-    pairs_dict = build_pairs_rq(queryfile,reffile)
-elif args.queryfile is not None and args.reffile is None:
-    sys.exit(('You must submit a query file and reference file together. '
-              'Exiting.'))
-elif args.samplefile is not None:
-    pairs_dict = build_pairs_all(samplefile)
-else:
-    sys.exit('Error with data file specification on the command line. Exiting.')
+    if args.queryfile and args.reffile is not None:
+        pairs_dict = build_pairs_rq(queryfile,reffile)
+    elif args.queryfile is not None and args.reffile is None:
+        sys.exit(('You must submit a query file and reference file together. '
+                  'Exiting.'))
+    elif args.samplefile is not None:
+        pairs_dict = build_pairs_all(samplefile)
+    else:
+        sys.exit('Error with data file specification on the command line. Exiting.')
 
-files_dict = write_submission_files(pairs_dict, submissions_dir, maxrefs = 75)
-submit_ggdc_jobs(bruteforce, wait,
-                 status_path, submit_path,
-                 files_dict, email, blastVariant)
+    files_dict = write_submission_files(pairs_dict,
+                                        submissions_dir,
+                                        maxrefs = 75)
+    submit_ggdc_jobs(bruteforce, wait,
+                     status_path, submit_path,
+                     files_dict, email, blastVariant)
+
+if __name__ == "__main__":
+    main(args = args)
